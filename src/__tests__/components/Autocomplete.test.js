149246acf5cfa9d48b32511a4699d62138dc5ad4
Bart Kalanski <bkalanski@gmail.com>
2021-04-15 19:47:24 +0100

    Split SearchBox component

--- src/__tests__/components/Autocomplete.test.js
+++ src/__tests__/components/Autocomplete.test.js
@@ -9,4 +9,56 @@ describe("Autocomplete", () => {
     wrapper = shallow(<Autocomplete />);
     expect(wrapper).not.toBeNull();
   })
+  it('should not display any results if input value length is less than two', () => {
+    wrapper = shallow(<Autocomplete value="a" results={[]}/>);
+    const results = wrapper.find(".results__results");
+    expect(results.children()).toHaveLength(0);
+  })
+  it('should display no results found message', () => {
+    wrapper = shallow(<Autocomplete value="xxx" results={[{ name: 'No results found' }]} />);
+    const result = wrapper.find(".results__error");
+    expect(result.text()).toBe('No results found');
+  })
+  it('should display results if input value length is more than or equal to two', () => {
+    let mockResults = [{
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    }, {
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    }, {
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    }, {
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    }, {
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    }, {
+      name: '',
+      bookingId: '',
+      city: '',
+      region: '',
+      country: '',
+    },]
+    wrapper = shallow(<Autocomplete value="abc" results={mockResults} />);
+    const result = wrapper.find(".results__result");
+    expect(result).toHaveLength(6);
+  })
 })
